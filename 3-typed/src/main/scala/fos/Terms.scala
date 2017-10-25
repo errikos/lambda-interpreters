7ec224a307737f3d6391a8cbe7501471e05790f7
Ergys Dona <errikosd@gmail.com>
2017-10-25 10:58:04 +0200

    as3: apply idea hints
    * add type annotations
    * remove redundant parentheses
    
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- 3-typed/src/main/scala/fos/Terms.scala
+++ 3-typed/src/main/scala/fos/Terms.scala
@@ -6,67 +6,67 @@ import scala.util.parsing.input.Positional
 abstract class Term extends Positional
 
 case class True() extends Term {
-  override def toString() = "true"
+  override def toString = "true"
 }
 
 case class False() extends Term {
-  override def toString() = "false"
+  override def toString = "false"
 }
 case class Zero() extends Term {
-  override def toString() = "0"
+  override def toString = "0"
 }
 case class Succ(t: Term) extends Term {
-  override def toString() = "succ " + t
+  override def toString: String = "succ " + t
 }
 case class Pred(t: Term) extends Term {
-  override def toString() = "pred " + t
+  override def toString: String = "pred " + t
 }
 case class IsZero(t: Term) extends Term {
-  override def toString() = "iszero " + t
+  override def toString: String = "iszero " + t
 }
 case class If(cond: Term, t1: Term, t2: Term) extends Term {
-  override def toString() = "if " + cond + " then " + t1 + " else " + t2
+  override def toString: String = "if " + cond + " then " + t1 + " else " + t2
 }
 
 case class Var(name: String) extends Term {
-  override def toString() = name
+  override def toString: String = name
 }
 case class Abs(v: String, tp: Type, t: Term) extends Term {
-  override def toString() = "(\\" + v + ":" + tp + "." + t + ")"
+  override def toString: String = "(\\" + v + ":" + tp + "." + t + ")"
 }
 case class App(t1: Term, t2: Term) extends Term {
-  override def toString() = (t1 match {
+  override def toString: String = (t1 match {
     case App(_, _) => " (" + t1.toString + ")" // left-associative
     case _         => t1.toString
   }) + " " + t2.toString
 }
 case class TermPair(t1: Term, t2: Term) extends Term {
-  override def toString() = "{" + t1 + "," + t2 + "}"
+  override def toString: String = "{" + t1 + "," + t2 + "}"
 }
 
 case class First(t: Term) extends Term {
-  override def toString() = "fst " + t
+  override def toString: String = "fst " + t
 }
 
 case class Second(t: Term) extends Term {
-  override def toString() = "snd " + t
+  override def toString: String = "snd " + t
 }
 
 /** Abstract Syntax Trees for types. */
 abstract class Type extends Positional
 
 case object TypeBool extends Type {
-  override def toString() = "Bool"
+  override def toString = "Bool"
 }
 case object TypeNat extends Type {
-  override def toString() = "Nat"
+  override def toString = "Nat"
 }
 case class TypeFun(t1: Type, t2: Type) extends Type {
-  override def toString() = t1.toString + "->" + (t2 match {
+  override def toString: String = t1.toString + "->" + (t2 match {
     case TypeFun(_, _) => "(" + t2.toString + ")" // right-associative
     case _             => t2.toString
   })
 }
 case class TypePair(t1: Type, t2: Type) extends Type {
-  override def toString() = "(" + t1 + " * " + t2 + ")"
+  override def toString: String = "(" + t1 + " * " + t2 + ")"
 }
